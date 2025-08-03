b92346309276adfde2c07f17095e5ce12968479d
Ludvig Hult <ludvig.hult@gmail.com>
2025-08-03 20:40:01 +0200

    formatting

--- day19.py
+++ day19.py
@@ -1,42 +1,38 @@
-
 from util import read_input
 from re import compile
 from typing import Literal
 
 my_input = read_input(19)
-CHR = 'c'
-SEQ = '>'
-ALT = '|'
+CHR = "c"
+SEQ = ">"
+ALT = "|"
 
 
 rules = {}
 for line in my_input.splitlines():
   line = line.strip()
-  if line == '':
-    break # end of rules
-  rule_num, rhs = line.split(': ')
+  if line == "":
+    break  # end of rules
+  rule_num, rhs = line.split(": ")
   rule_num = int(rule_num)
-  
+
   if rhs.startswith('"'):
     assert len(rhs) == 3
     char = rhs[1]
     rules[rule_num] = (CHR, char)
     continue
 
-  if '|' in rhs:
-    ors = rhs.split(' | ')
+  if "|" in rhs:
+    ors = rhs.split(" | ")
     assert len(ors) == 2
-    alt1  = ('>', *map(int, ors[0].split(' ')))
-    alt2 = ('>', *map(int, ors[1].split(' ')))
-    alts = ('|', alt1, alt2
-            )
-    rules[rule_num] = alts    
+    alt1 = (">", *map(int, ors[0].split(" ")))
+    alt2 = (">", *map(int, ors[1].split(" ")))
+    alts = ("|", alt1, alt2)
+    rules[rule_num] = alts
     continue
-  
-  seq = tuple(map(int, rhs.split(' ')))
-  rules[rule_num] = ('>', *seq)
 
-
+  seq = tuple(map(int, rhs.split(" ")))
+  rules[rule_num] = (">", *seq)
 
 
 # build a regex for rule 0 using the rule set above
@@ -47,7 +43,7 @@ def build_regex(rule_num: int) -> str:
     assert len(rule) == 2
     return rule[1]
   elif rule[0] == SEQ:
-    out = ''
+    out = ""
     for k in rule[1:]:
       out += build_regex(k)
     return out
@@ -55,16 +51,14 @@ def build_regex(rule_num: int) -> str:
     assert len(rule) == 3
     out1 = "".join(build_regex(k) for k in rule[1][1:])
     out2 = "".join(build_regex(k) for k in rule[2][1:])
-    out = f'({out1}|{out2})'
+    out = f"({out1}|{out2})"
     return out
   else:
-    raise ValueError(f'Unknown rule type {rule[0]} for rule {rule_num}')
+    raise ValueError(f"Unknown rule type {rule[0]} for rule {rule_num}")
 
 
+regex = compile("^" + build_regex(0) + "$")
 
-regex = compile('^' + build_regex(0) + '$')
-
-messages = my_input.splitlines()[len(rules):]
-ans_one = sum(1 for m in messages if regex.match(m)
-              )
-assert ans_one == 120, 'First try!'
+messages = my_input.splitlines()[len(rules) :]
+ans_one = sum(1 for m in messages if regex.match(m))
+assert ans_one == 120, "First try!"
