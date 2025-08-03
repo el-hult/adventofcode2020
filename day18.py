152503266628281893f3e7f4e0e432974eb6181c
Ludvig Hult <ludvig.hult@gmail.com>
2025-08-03 16:22:45 +0200

    code cleanup

--- day18.py
+++ day18.py
@@ -1,71 +1,87 @@
 
 from util import read_input
 
+def eval_line(line:str, part:int) -> int:
+  """Evaluate the maths expression in `line` according to the rules of part `part`."""
+  MUL = '*'
+  ADD = '+'
+  LPAR = '('
+  RPAR = ')'
+  PREC = {ADD: 2, MUL: 1} if part == 2 else {ADD: 1, MUL: 1}
+  BINOPS = {ADD, MUL}
 
-class PartOneSolver:
-  def __init__(self, expression: str):
-    self.e = expression
-    self.p = 0  # pointer in expression
-
-  def consume_num(self) -> int:
-    """Consume a number from the expression."""
-    start = self.p
-    while self.p < len(self.e) and self.e[self.p].isdigit():
-      self.p += 1
-    return int(self.e[start:self.p])
-  
-  def consume_value(self) -> int:
-    """Consume a value, which is either a number or an expression in parentheses.
-    Parenthesized expressions are evaluated"""
-    if self.e[self.p] == '(':
-      self.p += 1  # skip '('
-      value = self.consume_expression()
-      self.p += 1  # skip ')'
-      return value
+  # Tokenize the input expression
+  token_stream = []
+  eptr = 0  # pointer into expression
+  while eptr < len(line):
+    if line[eptr] == ' ':
+      eptr += 1
+    elif line[eptr] == '+':
+      token_stream.append(ADD)
+      eptr += 1
+    elif line[eptr] == '*':
+      token_stream.append(MUL)
+      eptr += 1
+    elif line[eptr] == '(':
+      token_stream.append(LPAR)
+      eptr += 1
+    elif line[eptr] == ')':
+      token_stream.append(RPAR)
+      eptr += 1
+    elif line[eptr].isdigit():
+      val = int(line[eptr])
+      eptr += 1
+      while eptr < len(line) and line[eptr].isdigit():
+        val = val * 10 + int(line[eptr])
+        eptr += 1
+      token_stream.append(val)
     else:
-      return self.consume_num()
-    
-  def consume_op(self) -> str:
-    """Consume an operator from the expression."""
-    if self.e[self.p:self.p + 3] == ' + ':
-      self.p += 3
-      return '+'
-    elif self.e[self.p:self.p + 3] == ' * ':
-      self.p += 3
-      return '*'
+      raise ValueError(f"Unexpected character in expression: {line[eptr]} at position {eptr}")
+
+  # Convert token stream to Reverse Polish Notation (RPN) using the shunting yard algorithm
+  operator_stack = []
+  rpn_queue = []
+  for tkn in token_stream:
+    if isinstance(tkn, int):
+      rpn_queue.append(tkn)
+    elif tkn == LPAR:
+      operator_stack.append(tkn)
+    elif tkn == RPAR:
+      while operator_stack[-1] != LPAR:
+        rpn_queue.append(operator_stack.pop())
+      assert operator_stack[-1] == LPAR
+      operator_stack.pop()
+    elif tkn in BINOPS:
+      o1 = tkn # current operator
+      while operator_stack and (o2 := operator_stack[-1]) != LPAR and PREC[o2] >= PREC[o1]:
+        rpn_queue.append(operator_stack.pop()) # pop o2 
+      operator_stack.append(o1)  # push the current operator onto the stack
+  while operator_stack:
+    assert operator_stack[-1] != LPAR, "Unexpected '(' on opstack at end of parsing"
+    rpn_queue.append(operator_stack.pop()) # drain the operator stack at the end
+
+  # Now output is in reverse polish notation, and we can evaluate it
+  eval_stack = []
+  for tkn in rpn_queue:
+    if isinstance(tkn, int):
+      eval_stack.append(tkn)
+    elif tkn == ADD:
+      right = eval_stack.pop()
+      left = eval_stack.pop()
+      eval_stack.append(left + right)
+    elif tkn == MUL:
+      right = eval_stack.pop()
+      left = eval_stack.pop()
+      eval_stack.append(left * right)
     else:
-      raise ValueError(f"Unexpected operator at position {self.p}: '{self.e[self.p:self.p + 3]}'")
+      raise ValueError(f"Unexpected token in RPN queue: {tkn}")
     
-  def done(self) -> bool:
-    """Completed parsing expression?"""
-    return self.p >= len(self.e) or self.e[self.p] == ')'
-  
-  def consume_expression(self) -> int:
-    """An expressions is a value followed by zero or more operators and values.
-    
-    Two modes available:
-    - 'ltr': addition and multiplication are done left-to-right, no precedence.
-    - 'advanced': addition must be evaluated before multiplication, but parentheses must be evaluated first.
-    """
-    lhs = self.consume_value()
-    while not self.done():
-      op = self.consume_op()
-      rhs = self.consume_value()
-      lhs = lhs + rhs if op == '+' else lhs * rhs
-    return lhs
+  assert len(eval_stack) == 1
+  return eval_stack[0]
 
-def part_one(expression:str):
-  """Evaluate special maths.
-  addition and multiplication must be done left-to-right, not with precedence.
-  parentheses must be evaluated first.
-
-  plus and times have spaces around them
-  """
-  p = PartOneSolver(expression)
-  val = p.consume_expression()
-  assert p.done(), f"Did not consume whole expression: {expression} at position {p.p}"
-
-  return val
+def part_one(raw_input: str) -> int:
+  """Evaluate the input expression according to part one rules."""
+  return sum(eval_line(line.strip(), 1) for line in raw_input.splitlines())
 
 assert part_one("1 + 2 * 3 + 4 * 5 + 6") == 71
 assert part_one("1 + (2 * 3) + (4 * (5 + 6))") == 51
@@ -74,94 +90,13 @@ assert part_one("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 437
 assert part_one("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 12240
 assert part_one("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 13632
 
-my_input = read_input(18).strip().splitlines()
-res = 0 
-for line in my_input:
-  res += part_one(line.strip())
+my_input = read_input(18)
+res = part_one(my_input)
 assert res == 510009915468, "First try!"
 
-def part_two(parsed_expression_from_token_stream:str):
-  """For part two, I realized I should learn how to work with operator precedence and parentheses more properly.
-  The solution will be unnecessarily complicated, but it is more like a exercise for me in how to do this"""
+def part_two(raw_input: str) -> int:
+  return sum(eval_line(line.strip(), 2) for line in raw_input.splitlines())
 
-  token_stream = []
-  MUL = '*'
-  ADD = '+'
-  LPAR = '('
-  RPAR = ')'
-  END = '$'
-  eptr = 0 # pointer in expression
-  while eptr < len(parsed_expression_from_token_stream):
-    if parsed_expression_from_token_stream[eptr] == ' ':
-      eptr += 1
-    elif parsed_expression_from_token_stream[eptr] == '+':
-      token_stream.append(ADD)
-      eptr += 1
-    elif parsed_expression_from_token_stream[eptr] == '*':
-      token_stream.append(MUL)
-      eptr += 1
-    elif parsed_expression_from_token_stream[eptr] == '(':
-      token_stream.append(LPAR)
-      eptr += 1
-    elif parsed_expression_from_token_stream[eptr] == ')':
-      token_stream.append(RPAR)
-      eptr += 1
-    elif parsed_expression_from_token_stream[eptr].isdigit():
-      val = int(parsed_expression_from_token_stream[eptr])
-      eptr += 1
-      while eptr < len(parsed_expression_from_token_stream) and parsed_expression_from_token_stream[eptr].isdigit():
-        val = val * 10 + int(parsed_expression_from_token_stream[eptr])
-        eptr += 1
-      token_stream.append(val)
-    else:
-      raise ValueError(f"Unexpected character in expression: {parsed_expression_from_token_stream[eptr]} at position {eptr}")
-  token_stream.append(END)  # end of expression marker
-
-
-
-  # Run shunting yard to create a reverse polish notation expression
-  operator_stack = []  # stack for operators
-  out_queue = []
-  PREC = {ADD: 2, MUL: 1}
-  for tkn in token_stream:
-    if isinstance(tkn, int):
-      out_queue.append(tkn)
-    elif tkn == LPAR:
-      operator_stack.append(tkn)
-    elif tkn == RPAR:
-      while operator_stack[-1] != LPAR:
-        out_queue.append(operator_stack.pop())
-      assert operator_stack[-1] == LPAR, "Expected '(' on stack"
-      operator_stack.pop()
-    elif tkn in (ADD, MUL): # the only operators we have :)
-      o1 = tkn # current operator
-      while operator_stack and (o2 := operator_stack[-1]) != LPAR and PREC[o2] >= PREC[o1]:
-        out_queue.append(operator_stack.pop()) # pop o2 
-      operator_stack.append(o1)  # push the current operator onto the stack
-  while operator_stack:
-    assert operator_stack[-1] != LPAR, "Unexpected '(' on opstack at end of parsing"
-    out_queue.append(operator_stack.pop()) # drain the operator stack at the end
-
-  # Now oq is in reverse polish notation, and we can evaluate it
-  stack = []
-  for tkn in out_queue:
-    if isinstance(tkn, int):
-      stack.append(tkn)
-    elif tkn == ADD:
-      right = stack.pop()
-      left = stack.pop()
-      stack.append(left + right)
-    elif tkn == MUL:
-      right = stack.pop()
-      left = stack.pop()
-      stack.append(left * right)
-    else:
-      raise ValueError(f"Unexpected token in output queue: {tkn}")
-    
-  assert len(stack) == 1
-  return stack[0]
-
-    
 assert part_two("1 + 2 * 3 + 4 * 5 + 6") == 231
 assert part_two("1 + (2 * 3) + (4 * (5 + 6))") == 51
 assert part_two("2 * 3 + (4 * 5)") == 46
@@ -169,5 +104,5 @@ assert part_two("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 1445
 assert part_two("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 669060
 assert part_two("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 23340
 
-res2 = sum(part_two(line.strip()) for line in my_input)
+res2 = part_two(my_input)
 assert res2 == 321176691637769, "First try!"
--- readme.md
+++ readme.md
@@ -36,6 +36,8 @@ Part one, I could implement myself with a simple state machine, because one only
 
 For part two, I read up a little on PRATT parsers, operator climbing, and finally found the shunting yard algorithm. The last one was the simplest, but still solved my problem. Because there are no unary operators, a small simplification was possible. I created the intermediate Reverse Polish Notation expression for debugging as well, even though it was not really needed. I suppose I could have saved a tiny bit of memory there. The algorithm is not terribly interesting, and I cannot say I really learned it. 
 
+With the part_two solver, I could solve part_one with same code by just changing operator precedence, simplifying my code a little.
+
 # Day 17
 Day 17 is about running cellular automata in 3D and 4D. At first, I got the wrong answer, and was very confused. After rewriting a bit, making the code simpler (at the cost of a little performance) I got the right answer. Instead of 
 ```
